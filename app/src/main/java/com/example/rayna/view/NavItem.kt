1cfc846e063a7b16c9bd4dbc334db9929e4b51f3
mammasai <mammsai4@gmail.com>
2025-02-27 10:41:22 +0300

    dd and modify project files

---
+++ app/src/main/java/com/example/rayna/view/NavItem.kt
@@ -0,0 +1,10 @@
+package com.example.rayna.view
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavItem(
+    val label: String,
+    val icon: ImageVector,
+    val route: String,
+)
+
